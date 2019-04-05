7bd3af0e967bca665b181f8c8a7dbc4c9de91377
Pedro Pablo Gallego Pinzon - Ceiba Software <pedro.gallego@ceiba.com.co>
2019-04-05 07:05:48 -0500

    Configure ic

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,6 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-
+process.env.CHROME_BIN = require('puppeteer').executablePath()
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -25,11 +25,11 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless', 'Chrome'],
     customLaunchers: {
-      ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
+      Chrome_without_security: {
+        base: 'Chrome',
+        flags: ['--disable-web-security']
       }
     },
     singleRun: false,
